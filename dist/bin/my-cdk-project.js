ab8e05999af9544811dc1c2dbf9cf996fd9cae64
Sahana Yogeesh <sahanajathan@gmail.com>
2024-12-04 17:25:39 +0530

    Update my-cdk-project.js

--- dist/bin/my-cdk-project.js
+++ dist/bin/my-cdk-project.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { App } = require('aws-cdk-lib');
-const { MyCdkStack } = require('../lib/my-cdk-stack');  // Import your stack
+const { MyCdkStack } = require('../lib/my-cdk-project-stack');  // Import your stack
 
 const app = new App();
 new MyCdkStack(app, 'MyCdkStack');
