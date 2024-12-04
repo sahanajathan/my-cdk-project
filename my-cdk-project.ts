12d4a6983c5b891e32744ee42e4079c90879e50f
Sahana Yogeesh <sahanajathan@gmail.com>
2024-12-04 15:00:42 +0530

    Delete my-cdk-project.ts

--- my-cdk-project.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { MyCdkProjectStack } from './lib/my-cdk-project-stack';
-
-// Create the CDK app
-const app = new cdk.App();
-
-// Instantiate the stack and add it to the app
-new MyCdkProjectStack(app, 'MyCdkProjectStack');
