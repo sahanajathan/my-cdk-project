c16ba707081decd8c05b36a225a388e5e8818c31
Sahana Yogeesh <sahanajathan@gmail.com>
2024-12-04 10:53:18 +0530

    Create my-cdk-project.ts

---
+++ bin/my-cdk-project.ts
@@ -0,0 +1,8 @@
+import * as cdk from 'aws-cdk-lib';
+import { MyCdkProjectStack } from './lib/my-cdk-project-stack';
+
+// Create the CDK app
+const app = new cdk.App();
+
+// Instantiate the stack and add it to the app
+new MyCdkProjectStack(app, 'MyCdkProjectStack');
