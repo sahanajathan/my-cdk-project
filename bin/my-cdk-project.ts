748e385ebe9b5fc2c973a70a5d603258cd6c1106
Sahana Yogeesh <sahanajathan@gmail.com>
2024-12-04 16:01:06 +0530

    Update my-cdk-project.ts

--- bin/my-cdk-project.ts
+++ bin/my-cdk-project.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { MyCdkProjectStack } from '../lib/my-cdk-project-stack.js';  // Ensure the .js extension for ES Modules
+import { MyCdkProjectStack } from '../lib/my-cdk-project-stack';
 
+// Create the CDK app
 const app = new cdk.App();
+
+// Instantiate the stack and add it to the app
 new MyCdkProjectStack(app, 'MyCdkProjectStack');
